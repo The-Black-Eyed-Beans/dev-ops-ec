3f3b93aaf03d63481ac3896d190bc28fe01bce03
Ethan Carson <ethan.carson@smoothstack.com>
2022-03-09 14:34:39 -0500

    Refactor workspace

--- TF/Modules/ECS/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "app_name" {
-  type        = string
-  description = "Name of application (used to label components)"
-}
--- TF/Modules/EKS/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "app_name" {
-  type        = string
-  description = "Name of application (used to label components)"
-}
-
-variable "private_subnets" {
-  type        = list(string)
-  description = "List of private subnet cidrs"
-}
